f6dc339cb277e02741daa6abb1cfa0ef83fd410d
Charlotte Corapi <cahcorapi@gmail.com>
2026-02-19 15:49:53 +0100

    made file path in main system-independent and set up requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os
+
 from src.api.llm_client import LlamaClient
 from src.utils.file_loader import load_file
 
-text = load_file("src\\documents\\test.txt")
+file_path = os.path.join("src", "documents", "test.txt")
+text = load_file(file_path)
 
 prompt = text + "\n\n Can you identify the epics in the document?"
 
