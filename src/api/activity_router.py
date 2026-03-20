33e12dbf721778664c929e5f284f4965a25444da
Henrik André Gaustad <hengau@live.no>
2026-03-20 01:01:23 +0100

    Add router files.

---
+++ src/api/activity_router.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+from ..schemas.activity import ActivityResponseModel
+from ..database import get_db
+from fastapi import APIRouter, Depends
+from src.model.activity import Activity
+
+
+router = APIRouter(
+    prefix="/activities",
+    tags=["Activities"],
+)
+
+@router.get("/", response_model=list[ActivityResponseModel])
+def get_all_activities(db: Session = Depends(get_db)):
+    """Retrieve all activities from the database."""
+    return db.query(Activity).all()
