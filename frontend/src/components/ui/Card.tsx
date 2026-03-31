45ad84da0bf3b3c4ffeccdd7950e93edc5517875
Charlotte Corapi <cahcorapi@gmail.com>
2026-03-31 12:32:57 +0200

    cleanup

--- frontend/src/components/ui/Card.tsx
+++
@@ -1,9 +0,0 @@
-import type { HTMLAttributes } from "react";
-import { cardBase } from "../../styles/tokens";
-
-export function Card({
-  style,
-  ...props
-}: HTMLAttributes<HTMLDivElement>) {
-  return <div style={{ ...cardBase, ...style }} {...props} />;
-}
