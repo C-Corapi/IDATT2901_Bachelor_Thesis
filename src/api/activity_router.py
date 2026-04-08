65125b4b7d79d23a6e8ea59154f3e12642595542
Henrik André Gaustad <hengau@live.no>
2026-04-08 21:27:45 +0200

    formatting

--- src/api/activity_router.py
+++ src/api/activity_router.py
@@ -27,53 +27,46 @@ def extract_and_save_activities(filepath: str, db: Session = Depends(get_db)):  
     saved: list[Activity] = save_activities_to_db(activities, db)
     return saved
 
+
 @router.post("/", response_model=ActivityResponseModel, status_code=201)
 def create_activity(activity: ActivityCreateModel, db: Session = Depends(get_db)):  # type: ignore[assignment]
     """Create a new activity in the database."""
     return save_activities_to_db([activity], db)[0]
 
+
 @router.get("/{activity_id}", response_model=ActivityResponseModel)
 def get_activity(activity_id: int, db: Session = Depends(get_db)):  # type: ignore[assignment]
     """Retrieve a specific activity by its ID."""
-    activity: Activity | None = (
-        db.query(Activity)
-        .filter(Activity.id == activity_id)
-        .first()
-    )
+    activity: Activity | None = db.query(Activity).filter(Activity.id == activity_id).first()
 
     if activity is None:
         raise HTTPException(status_code=404, detail="Activity not found")
-    
+
     return activity
+
 
 @router.delete("/{activity_id}", status_code=204)
 def delete_activity(activity_id: int, db: Session = Depends(get_db)):  # type: ignore[assignment]
     """Delete a specific activity by its ID."""
-    activity: Activity | None = (
-        db.query(Activity)
-        .filter(Activity.id == activity_id)
-        .first()
-    )
+    activity: Activity | None = db.query(Activity).filter(Activity.id == activity_id).first()
 
     if activity is None:
         raise HTTPException(status_code=404, detail="Activity not found")
-    
+
     db.delete(activity)
     db.commit()
 
+
 @router.put("/{activity_id}", response_model=ActivityResponseModel)
-def update_activity(activity_id: int, updated_activity: ActivityCreateModel,
-                    db: Session = Depends(get_db)):  # type: ignore[assignment]
+def update_activity(
+    activity_id: int, updated_activity: ActivityCreateModel, db: Session = Depends(get_db)
+):  # type: ignore[assignment]
     """Update a specific activity by its ID."""
-    activity: Activity | None = (
-        db.query(Activity)
-        .filter(Activity.id == activity_id)
-        .first()
-    )
+    activity: Activity | None = db.query(Activity).filter(Activity.id == activity_id).first()
 
     if activity is None:
         raise HTTPException(status_code=404, detail="Activity not found")
-    
+
     for key, value in updated_activity.model_dump().items():
         setattr(activity, key, value)
 
