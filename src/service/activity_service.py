ad35ec2b58220490ffc4e62afef793ff8b7f0cba
Henrik <hengau@live.no>
2026-04-23 15:58:43 +0200

    refactor acitivity_router

--- src/service/activity_service.py
+++ src/service/activity_service.py
@@ -5,11 +5,14 @@ from typing import Any
 
 from sqlalchemy.orm import Session
 
+from exceptions.common import ActivityNotFound
 from model.activity import Activity
 from prompts.activity_extraction import ACTIVITY_EXTRACTION_PROMPT
 from schemas.activity import ActivityCreateModel
 from utils.file_loader import load_file
 from utils.llm_client import LlamaClient
+
+from repository import acitvity_repository
 
 
 def extract_activities(filepath: str) -> list[ActivityCreateModel]:
@@ -37,3 +40,67 @@ def save_activities_to_db(activities: list[ActivityCreateModel], db: Session) ->
     for activity in db_activities:
         db.refresh(activity)
     return db_activities
+
+def get_all_activities(db: Session) -> list[Activity]:
+    """Retrieve all activities from the database.
+    
+    Args:
+        db (Session): The database session.
+
+    Returns:
+        list[Activity]: A list of all activities in the database.
+    """
+    return acitvity_repository.get_all(db)
+
+def get_activity(db: Session, activity_id: int) -> Activity:
+    """Retrieve an activity by its ID.
+    
+    Args:
+        db (Session): The database session.
+        activity_id (int): The ID of the activity to retrieve.
+    
+    Returns:
+        Activity: The activity with the specified ID.
+
+    Raises:
+        ActivityNotFound: If no activity with the specified ID is found.
+    """
+    activity: Activity | None = acitvity_repository.get_by_id(db, activity_id)
+
+    if activity is None:
+        raise ActivityNotFound()
+    
+    return activity
+
+def delete_activity(db: Session, activity_id: int) -> bool:
+    """Delete an activity by its ID.
+
+    Args:
+        db (Session): The database session.
+        activity_id (int): The ID of the activity to delete.
+    
+    Returns:
+        bool: True if an activity was deleted, False otherwise.
+    """
+    return acitvity_repository.delete(db, activity_id)
+
+def update_activity(db: Session, activity_id: int, updated_activity: ActivityCreateModel) -> Activity:
+    """Update an activity by its ID.
+
+    Args:
+        db (Session): The database session.
+        activity_id (int): The ID of the activity to update.
+        updated_activity (ActivityCreateModel): The updated activity data.
+
+    Returns:
+        Activity: The updated activity.
+
+    Raises:
+        ActivityNotFound: If no activity with the specified ID is found.
+    """
+    activity: Activity | None = acitvity_repository.update(db, activity_id, updated_activity)
+
+    if activity is None:
+        raise ActivityNotFound()
+    
+    return activity
